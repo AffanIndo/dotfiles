d7fb402de1563eb06153cc10dce815dd687c109f
AffanIndo <AffanIndo@users.noreply.github.com>
2020-04-14 21:32:07 +0700

    zsh add path

--- zsh/.zsh/export.zsh
+++ zsh/.zsh/export.zsh
@@ -2,6 +2,9 @@
 export LESSHISTFILE=/dev/null # Disable .lesshst
 export EDITOR=/usr/bin/vim
 
+# Path
+PATH=$PATH:~/.local/bin
+
 # FZF (require FZF and fd)
 export FZF_DEFAULT_COMMAND='fd --type f --follow --exclude node_modules --exclude "*RECYCLE.BIN"'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
